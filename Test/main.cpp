dc1084c45c013aa55ccab097494e5932e12f20c2
Vladimir <48993600+MrApple163@users.noreply.github.com>
2019-04-26 19:14:49 +0300

    hm

---
+++ Test/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Input first number >> ";
+    cin >> a;
+    cout << "Input second number >> ";
+    cin >> b;
+    cout << "\nSum is >> " << a + b;
+    return 0;
+}
